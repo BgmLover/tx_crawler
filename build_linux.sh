2191e6c74a77a09e94e76da086d43f9356123e1a
BgmLover <3150105495@zju.edu.cn>
2019-04-09 14:04:48 +0800

    update build.sh

---
+++ build_linux.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo pip3 install requests;
+sudo pip3 install beautifulsoup4;
+sudo pip3 install lxml;
--- build_osx.sh
+++ build_osx.sh
@@ -2,3 +2,4 @@
 brew install sqlite3;
 pip3 install requests;
 pip3 install beautifulsoup4;
+pip3 install lxml;
